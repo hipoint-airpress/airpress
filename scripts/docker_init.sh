f32021d32637d05249115086f65ea22085fe12c1
rfancn <reg_info@qq.com>
2026-09-20 10:38:39 +0800

    fix docker_init.sh

--- scripts/docker_init.sh
+++ scripts/docker_init.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 check_and_copy() {
-    if [ ! -e /airpress/$1 ]; then
-        mkdir -p /airpress/$1
-        cp -Rf /app/$1/* /airpress/$1/
+    if [ ! -e /app/$1 ]; then
+        mkdir -p /app/$1
+        cp -Rf /app/$1/* /app/$1/
     fi
 }
 
 make_and_copy() {
-    mkdir -p /airpress/$1
-    cp -Rf /app/$1/* /airpress/$1/
+    mkdir -p /app/$1
+    cp -Rf /app/$1/* /app/$1/
 }
 
 make_and_copy 'resources/admin'
