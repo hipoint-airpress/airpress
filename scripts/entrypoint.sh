56494074259ea270bc712762cd93a6dfb86d72dc
rfancn <reg_info@qq.com>
2026-09-20 15:36:22 +0800

    fix Dockerfile missing package

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -6,10 +6,10 @@ IMAGE_RESOURCE_DIR="/resources"
 HOST_RESOURCE_DIR="/app/resources"
 
 mkdir -p "$HOST_RESOURCE_DIR"
-if [ -d "$DIR_IMAGE_RESOURCE_DIR" ]; then
+if [ -d "$IMAGE_RESOURCE_DIR" ]; then
     echo "Copy default resources from $IMAGE_RESOURCE_DIR to $HOST_RESOURCE_DIR..."
     # 递归复制，但不覆盖宿主机已有文件
-    cp -fr "IMAGE_RESOURCE_DIR"/* "$HOST_RESOURCE_DIR"/
+    cp -fr "$IMAGE_RESOURCE_DIR"/* "$HOST_RESOURCE_DIR"/
 fi
 
 echo "Start AirPress..."
